79dfe0de3ae9e9a32d44a3abb6ace24cb0edbc9b
swapnilk30 <sk9137587676@gmail.com>
2023-11-06 02:45:06 +0530

    added dependency spring-boot-starter-data-jpa

--- src/main/java/com/algowebpro/entity/User.java
+++ src/main/java/com/algowebpro/entity/User.java
@@ -1,7 +1,14 @@
 package com.algowebpro.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class User {
 
+    @Id
     private String userId;
     private String name;
 }
